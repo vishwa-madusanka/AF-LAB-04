0841eaf8ae505c32e4f2e3a5599a4f025ebc47db
Vishwa Wijekoon <vishwamadusanka1022@gmail.com>
2026-02-21 15:05:27 +0530

    Add async await feature

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const https = require('https');
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-    let data = '';
-
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    resp.on('end', () => {
-        console.log(JSON.parse(data));
-    });
-}).on('error', (err) => {
-    console.log("Error: " + err.message);
+const condition = true; // Change this to false to see the rejection case
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+        resolve('Success!');
+    } else {
+        reject('Failure!');
+    }
 });
+
+myPromise.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
+
+async function myFunction() {
+    try {
+        const result = await myPromise;
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+myFunction();
