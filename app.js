ea3521eb5a4af2a33d274d3e82244fa335310ffc
Vishwa Wijekoon <vishwamadusanka1022@gmail.com>
2026-02-21 14:35:57 +0530

    Add read file feature

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Lab 04 Initial Setup Complete");
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
