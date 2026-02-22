41da4a713da19b17aa6d5ab71ab7d497ab0bb632
Vishwa Wijekoon <vishwamadusanka1022@gmail.com>
2026-02-22 11:11:45 +0530

    Add complete and combined Node.js examples for final submission

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
-const condition = false; // Change this to false to see the rejection case
+// 1. Read file
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+
+// 2. Write file
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+    if (err) throw err;
+    console.log('File saved!');
+});
+
+// 3. Creating a web server
+const http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!');
+    res.end();
+}).listen(8080);
+
+// 4. HTTP Request
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+    resp.on('end', () => {
+        console.log(JSON.parse(data));
+    });
+}).on('error', (err) => {
+    console.log("Error: " + err.message);
+});
+
+// 5. Using module
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
+
+// 6. Promises
+const condition = true;
 const myPromise = new Promise((resolve, reject) => {
     if (condition) {
         resolve('Success!');
@@ -13,7 +54,8 @@ myPromise.then((result) => {
     console.log(error);
 });
 
-async function myFunction() {
+// 7. Async/Await
+async function myAsyncFunction() {
     try {
         const result = await myPromise;
         console.log(result);
@@ -22,4 +64,4 @@ async function myFunction() {
     }
 }
 
-myFunction();
+myAsyncFunction();
